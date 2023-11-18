2dbff59cbb4030d8604da9df554e75cb31cd259c
Iain McManus <iainmcmanus@protonmail.com>
2023-11-19 09:51:54 +1000

    Part 4 - Brain

--- Runtime/HybridGOAP/TestScene/SimpleGOAPBrain.cs
+++ Runtime/HybridGOAP/TestScene/SimpleGOAPBrain.cs
@@ -6,6 +6,12 @@ namespace HybridGOAP
 {
     public class SimpleGOAPBrain : GOAPBrainBase
     {
+        protected override void ConfigureBlackboard()
+        {
+        }
 
+        protected override void ConfigureBrain()
+        {
+        }
     }
 }
